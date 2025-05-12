5c29b8cefe8598ea75b0e0778b778c90047d15a7
shashisurpur <shashisurpurv@gmail.com>
2025-05-13 01:00:04 +0530

    updated

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -4,6 +4,9 @@ const email = process.env.NEXT_PUBLIC_EMAIL;
 const pass = process.env.NEXT_PUBLIC_EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
+  host: "https://my-port-folio-nu-eight.vercel.app/",
+  port: 465,
+  secure: true,
   service: "gmail",
   auth: {
     user: process.env.NEXT_PUBLIC_EMAIL,
